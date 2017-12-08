5bf0f3243261763934a6de4b9fec35119193ac42
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2017-12-08 02:11:19 +0100

    added p2 paket 4 final

---
+++ src/dbwt/dbwt/App_Code/Beispielhelpers.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dbwt.App_Code
+{
+    public class BeispielhelpersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
