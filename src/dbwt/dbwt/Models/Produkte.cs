8830c1e7aa19c6939c277be14ca3072e6bb9750e
Unknown <henninggamerschlag@gmail.com>
2018-01-16 18:28:07 +0100

    carsten xml daten auslesen

---
+++ src/dbwt/dbwt/Models/Produkte.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace dbwt
+{
+
+    public class MenuList
+    {
+        public List<MenuProdukt> Produkte = null;
+        public string Motto { get; set; }
+
+        public class MenuProdukt
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+
+
+        public MenuList()
+        {
+            Produkte = new List<MenuProdukt>();
+        }
+
+        public void addProdukt(int id, string name)
+        {
+            MenuProdukt item = new MenuProdukt();
+            item.ID = id;
+            item.Name = name;
+
+            Produkte.Add(item);
+        }
+
+    }
+}
---
+++ src/dbwt/dbwt/Models/xml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Xml.Linq;
+
+namespace dbwt
+{
+    public class XMLFactory
+    {
+        string XMLFILE = ("./Speiseplan.xml");
+        private XElement root;
+
+        private bool open()
+        {
+            bool result = true;
+
+            try
+            {
+                var document = XDocument.Load(@XMLFILE);
+                root = document.Root;
+
+                System.Diagnostics.Debug.WriteLine("ok");
+            }
+            catch
+            {
+                System.Diagnostics.Debug.WriteLine("xml error");
+                result = false;
+            }
+
+            return result;
+        }
+
+        public XMLFactory()
+        {
+            getMenuList();
+        }
+
+        public MenuList getMenuList(int weekday = -1)
+        {
+            MenuList result = new MenuList();
+
+            if (weekday == -1)
+            {
+                weekday = (int)DateTime.Now.DayOfWeek;
+            }
+
+            if (open())
+            {
+                System.Diagnostics.Debug.WriteLine("weekday:" + weekday);
+
+                try
+                {
+                    var menu = from e in root.Descendants("Menu") where (int)(Convert.ToDateTime(e.Attribute("Tag").Value).DayOfWeek) == weekday select e;
+                    var kw = (from e in root.Descendants("Menu") where (int)(Convert.ToDateTime(e.Attribute("Tag").Value).DayOfWeek) == weekday select e.Attribute("Kalenderwoche")).ToList()[0].Value.ToString();
+                    var motto = (from e in menu.Elements() select e).ToList()[0].Value.ToString();
+                    var produkte = (from e in menu.Descendants("Produkte").Descendants("Produkt") select e).ToList();
+
+                    //Liste mit Ergebnissen füllen
+                    result.Motto = motto + " (KW " + kw + ")";
+
+                    for (int i = 0; i < produkte.Count(); i++)
+                    {
+                        result.addProdukt(
+                                Convert.ToInt32(produkte[i].Attribute("ProduktID").Value),
+                                produkte[i].Attribute("Typ").Value.ToString()
+                            );
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Fehler in XML Verarbeitung");
+                }
+
+            }
+
+            return result;
+
+        }
+    }
+}
